6e0f0e8a1b22cb0aa0ad8baf703cba1dae61bb89
geoffwithers <geoffwithers91@gmail.com>
2024-04-30 22:45:43 -0700

    finished navbar and header components

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,14 +7,14 @@ import { SubHeading } from '../../components';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
-      <SubHeading title="Chase the new flavor" />
-      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <SubHeading title="Welcome to Redlight" />
+      <h1 className='app__header-h1'>Craft Brewing, Our Way</h1>
       <p className='p__opensans' style={{ margin: '2rem 0'}}>My name's Geoff and I"m makin a website. It's goin okay.</p>
       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
     
     <div className='app__wrapper_img'>
-      <img src={images.welcome} alt="header-img"/>
+      <img src={images.mural} alt="header-img"/>
     </div>
   </div>
 );
