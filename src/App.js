8acc994aced21bcf595e4f942d9502cb5781ddfa
geoffwithers <geoffwithers91@gmail.com>
2024-04-30 18:16:01 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello, World!</h1>
+//     </div>
+//   );
+// }
+// export default App;
+
+import React from 'react';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
